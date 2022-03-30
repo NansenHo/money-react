b81965ad880ee1f504bb37628a6f79962dab8f4d
nansen <nansen.ho@foxmail.com>
2022-03-30 13:52:06 +0800

    react router 基本-有了三个路由&默认进入money页面&404页面路由匹配

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,65 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
 
 function App() {
-  return (
-    <div>Hello World!</div>
-  );
-}
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/tags">标签页</Link>
+              </li>
+              <li>
+                <Link to="/money">记账页</Link>
+              </li>
+              <li>
+                <Link to="/statistics">统计页</Link>
+              </li>
+            </ul>
+          </nav>
+  
+          <Switch>
+            <Route path="/tags">
+              <Tags />
+            </Route>
+            <Route path="/money">
+              <Money />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            <Redirect exact from="/" to="/money" />
+            <Route path="*">
+              <NoMatch />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
+  
+  function Tags() {
+    return <h2>Tags</h2>;
+  }
+  
+  function Money() {
+    return <h2>Money</h2>;
+  }
+  
+  function Statistics() {
+    return <h2>Statistics</h2>;
+  }
+
+  function NoMatch() {
+    return <h2>404</h2>;
+  }
+
 
 export default App;
