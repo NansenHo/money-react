69e919e87d11e9335a1daf8cf4585d1c59313c88
NansenHo <nansen.ho@foxmail.com>
2022-08-06 17:36:46 +0800

    :sparkles: money 页面的 tags 的点选功能

--- src/views/moneyPageStyledComponents/tagsSection.tsx
+++ src/views/moneyPageStyledComponents/tagsSection.tsx
@@ -21,6 +21,10 @@ const Wrapper = styled.section`
       padding: 3px 18px;
       margin: 6px 12px 0 12px;
       font-size: 14px;
+
+      &.selected {
+        background: yellow;
+      }
     }
   }
   > button {
@@ -33,15 +37,29 @@ const Wrapper = styled.section`
 
 // FC 是 functionComponent 的缩写
 const TagsSection: React.FC = (props) => {
+  // <> 括号里面写类型，() 里面写值
+  const [tags, setTags] = useState<string[]>(['衣', '食', '住', '行'])
+  const [selectedTags, setSelectedTags] = useState<string[]>([])
+  const addSelectedTags = (tag: string) => {
+    if (selectedTags.includes(tag)) {
+      setSelectedTags(selectedTags.filter(item => item !== tag))
+    } else {
+      setSelectedTags([...selectedTags, tag])
+    }
+  }
+  const getClass = (tag: string) => {return (selectedTags.includes(tag) ? 'selected' : '')}
   return (
     <Wrapper>
       <ul>
-        <li>衣</li>
-        <li>食</li>
-        <li>住</li>
-        <li>行</li>
-        <li>衣</li>
-        <li>食</li>
+        {tags.map(tag => {
+          // 如果接受的是一个函数 fn，那不能是一个执行函数 fn()
+          return <li key={tag}
+              onClick={()=>{addSelectedTags(tag)}}
+              // 如果接受的是一个字符串，里面就可以直接写执行函数 fn()，该函数返回一个字符串
+              className={getClass(tag)}>
+            {tag}
+          </li>
+        })}
       </ul>
       <button>新增标签</button>
     </Wrapper>
